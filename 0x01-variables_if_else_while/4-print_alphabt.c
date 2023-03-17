07bf71780dbb90a53eccb3dc991e4da8386859c8
ayoubamr1 <ayoubameur95@gmail.com>
2023-03-17 15:49:44 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Sucharess)
+*/
+int main(void)
+{
+	char cha;
+
+	cha = 'a';
+
+	while (cha <= 'z')
+	{
+		if (cha != 'e' && cha != 'q')
+		{
+			putchar (cha);
+		}
+		cha++;
+	}
+	putchar ('\n');
+	return (0);
+}
