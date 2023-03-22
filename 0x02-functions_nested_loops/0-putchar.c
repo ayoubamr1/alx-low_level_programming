21524e16876f23a1c6aa02a33dd5ebdd96d5f4fe
ayoubamr1 <ayoubameur95@gmail.com>
2023-03-21 17:12:47 -0700

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char name[8] = "_putchar";
+	int i;
+
+	for (i = 0, i < 8, i++)
+{
+	_putchar(name[i]);
+}
+	_putchar ('\n');
+	return (0);
+}
